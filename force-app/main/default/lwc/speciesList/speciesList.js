d71646990d6017314be9e6b9804a31adb2568037
Pipe_Bar <newpipe23@gmail.com>
2022-02-12 15:20:28 -0500

    Funcionalidades Añadidas

--- force-app/main/default/lwc/speciesList/speciesList.js
+++ force-app/main/default/lwc/speciesList/speciesList.js
@@ -4,7 +4,7 @@ import getAllSpec from "@salesforce/apex/SpeciesService.getAllSpecies";
 export default class SpeciesList extends LightningElement {
     @wire(getAllSpec)
     species;
-
+    //esta variable va a contener lo devuelto cuando @wire llame al método
     //species.data --> datos devueltos por apex
     //species.error --> devuelve la lista de posibles errores
 }
--- force-app/main/default/lwc/speciesTile/speciesTile.js
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -1,7 +1,33 @@
 import { LightningElement, api} from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class SpeciesTile extends LightningElement {
+export default class SpeciesTile extends NavigationMixin(LightningElement) {
     @api
     specie;
+
+    //specie.Location = 'Indoors'
+    //specie.Location = 'Outdoors'
+    //specie.Location = igual a los dos
+
+    get isOutdoors() {
+        return this.specie.Location__c.includes('OUTDOORS');
+    }
+    get isIndoors() {
+        return this.specie.Location__c.includes('INDOORS');
+    }
+
+    //navegar a la pagina de detalles del objeto para
+
+    navigateToRecordViewPage() {
+        // View a custom object record.
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.specie.Id,
+                objectApiName: 'Species__c', // objectApiName is optional
+                actionName: 'view'
+            }
+        });
+    }
 
 }
